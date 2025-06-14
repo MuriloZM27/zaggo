cb4edf42716d3fef268bb3d8694221d64efcea43
MuriloZM27 <mzmiotta@gmail.com>
2025-06-14 18:25:44 -0300

    corrigindo import

--- backend/src/main/java/com/zaggo/services/ReservaService.java
+++ backend/src/main/java/com/zaggo/services/ReservaService.java
@@ -1,6 +1,7 @@
 package com.zaggo.services;
 
 import com.zaggo.entities.Reserva;
+import com.zaggo.repositories.ReservaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +10,7 @@ import java.util.List;
 @Service
 public class ReservaService {
     @Autowired
-    private src.main.java.com.zaggo.repositories.ReservaRepository repo;
+    private ReservaRepository repo;
 
     public List<Reserva> listarPorUsuario(Long idUsuario) {
         return repo.findByUsuarioId(idUsuario);
