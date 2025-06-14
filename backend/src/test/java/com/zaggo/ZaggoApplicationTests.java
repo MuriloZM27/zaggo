dbbb443aea33b0cb0aabe835b60dd3cfc5ff67f3
MuriloZM27 <mzmiotta@gmail.com>
2025-06-14 16:48:26 -0300

    primeiro commit

---
+++ backend/src/test/java/com/zaggo/ZaggoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaggo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaggoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
